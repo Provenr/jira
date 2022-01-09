ccc3634ab44a50a836910120e733fb2c2e337aa9
Provenr <web_lyn@163.com>
2022-01-10 00:23:12 +0800

    docs: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          {" "}
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
@@ -16,6 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          {" "}
           Learn React
         </a>
       </header>
